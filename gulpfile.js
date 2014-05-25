35468f00f83eb1429788e2f41b3fd0e1a89c4bef
Rui Hu <code@mrhooray.com>
2014-05-25 14:41:10 -0700

    add run tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
+var mochaPhantomJS = require('./');
 
 var paths = {
   scripts: ['**/*.js', '!node_modules/**'],
@@ -21,6 +22,18 @@ gulp.task('test', ['jshint'], function () {
   return gulp
   .src(paths.tests)
   .pipe(mocha({reporter: 'spec'}));
+});
+
+gulp.task('run', function () {
+  return gulp
+  .src('test/fixture-pass.html')
+  .pipe(mochaPhantomJS({reporter: 'spec'}));
+});
+
+gulp.task('run:dump', function () {
+  return gulp
+  .src('test/fixture-pass.html')
+  .pipe(mochaPhantomJS({reporter: 'xunit', dump:'dump.xml'}));
 });
 
 gulp.task('default', ['test']);
