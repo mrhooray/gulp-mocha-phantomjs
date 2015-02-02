044a3bbe26913a893f972b0275ba286b2af3d684
Joshua Apple <joshua.apple@gmail.com>
2015-02-02 10:21:41 -0600

    Added relative path to phantomJS

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function crossPlatform(str) {
 
 function spawnPhantomJS(args, options, stream, cb) {
   // in case npm is started with --no-bin-links
-  var phantomjsPath = lookup('.bin/phantomjs', true) || lookup('phantomjs/bin/phantomjs', true);
+  var phantomjsPath = lookup('phantomjs/lib/phantom/phantomjs.exe') || lookup('.bin/phantomjs', true) || lookup('phantomjs/bin/phantomjs', true);
 
   if (!phantomjsPath) {
     return cb(new gutil.PluginError(pluginName, 'PhantomJS not found'));
